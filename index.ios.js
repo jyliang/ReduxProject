07b9978fee1c8c403c3e92ea0c2c6c5f5551dd8d
Jason Liang <liangjyjason@gmail.com>
2017-01-18 01:36:56 -0500

    configure flux store. empty reducer

--- index.ios.js
+++ index.ios.js
@@ -10,6 +10,26 @@ import { createStore, applyMiddleware, combineReducers, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 
+import reducer from './app/reducers'
+
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ });
+
+function configureStore(initialState) {
+  // create flux store
+  // enhancer is:
+  // a way of composing a bunch middleware, a bunch of functions
+  // that runs through each transformation state of the application
+  const enhancer = compose(
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware,
+    ),
+  );
+  return createStore(reducer, initialState, enhancer)
+}
+
+const store = configureStore({});
+
 import {
   AppRegistry,
   StyleSheet,
@@ -19,41 +39,20 @@ import {
 
 export default class ReduxProject extends Component {
   render() {
-    console.log("hello world");
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+      <View>
+        <Text>
+        basic
         </Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const App = () => (
+  <Provider store={store}>
+    <ReduxProject />
+  </Provider>
+);
 
-AppRegistry.registerComponent('ReduxProject', () => ReduxProject);
+AppRegistry.registerComponent('ReduxProject', () => App);
