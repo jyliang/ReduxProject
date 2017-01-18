70a632b5bd63a261fd6c509e13359da72ffbda5c
Jason Liang <liangjyjason@gmail.com>
2017-01-18 01:08:41 -0500

    Basic wiring of all the routes.

--- index.ios.js
+++ index.ios.js
@@ -6,7 +6,7 @@
 
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReduxers, compose} from 'redux'
+import { createStore, applyMiddleware, combineReducers, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 
